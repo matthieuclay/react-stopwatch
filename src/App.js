adb1f9ed329e987d11671cd29618fc8fe1e290cc
Matthieu Clay <matthieu.clay@outlook.fr>
2022-03-07 13:33:35 +0100

    :sparkles: add stopwatch function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  const [time, setTime] = useState(0);
+  const [timerOn, setTimerOn] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (timerOn) {
+      interval = setInterval(() => {
+        setTime((previousTime) => previousTime + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [timerOn]);
+
+  return (
+    <div id="App">
+      <h2>Stopwatch</h2>
+      <div className="display">
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      </div>
+
+      <div className="buttons">
+        {!timerOn && time === 0 && (
+          <button onClick={() => setTimerOn(true)}>Start</button>
+        )}
+        {timerOn && <button onClick={() => setTimerOn(false)}>Stop</button>}
+        {!timerOn && time > 0 && (
+          <button onClick={() => setTime(0)}>Reset</button>
+        )}
+        {!timerOn && time > 0 && (
+          <button onClick={() => setTimerOn(true)}>Resume</button>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
